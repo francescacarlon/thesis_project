2c9e20dc516679acde855cdf2e7ec059a02f92ce
francescacarlon <st185989@stud.uni-stuttgart.de>
2025-03-01 13:34:17 +0100

    test with prompts all llms

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ LLM_MODELS = {
     # "claude" : "claude",
     # "deepseek" : "deepseek"
     # "mistral" : "mistral"
-    # "llama" : "llama"    
+    "llama" : "llama"    
 }
 
 
@@ -17,27 +17,23 @@ def main():
     # TEST: ONLY N PROMPT 
 
     llm_model = "llama"
-    prompt_function_name = PROMPT_FUNCTIONS[1]  # Choose prompt n. to test
-
+    prompt_function_name = PROMPT_FUNCTIONS[5]  # Choose prompt n. to test
     print(f"\n📝 Running benchmark for:")
     print(f"   🧠 LLM Model: {llm_model}")
     print(f"   ✏️  Prompt Function: {prompt_function_name}")
-
     # ✅ Call the benchmark function for each LLM and each prompt
-    create_benchmark(llm_model, prompt_function_name, target_key="12") # max_entries=15) #,  # max_entries=from 1 to n. OR # target_key=only that key
+    create_benchmark(llm_model, prompt_function_name, target_key="3") # max_entries=15) #,  # max_entries=from 1 to n. OR # target_key=only that key
 
     # WHOLE BENCHMARK
     # for llm_model in LLM_MODELS.values():  # ✅ Only keep values (model names)
     #     for prompt_function_name in PROMPT_FUNCTIONS.values():  # ✅ Only keep function names
-# 
     #         print(f"\n📝 Running benchmark for:")
     #         print(f"   🧠 LLM Model: {llm_model}")
     #         print(f"   ✏️  Prompt Function: {prompt_function_name}")
-
-    # ✅ Call the benchmark function for each LLM and each prompt
-            # create_benchmark(llm_model, prompt_function_name) # WHOLE BENCHMARK
-
-    print("\n✅ All benchmarks have been generated successfully!")
+   ##  ✅ Call the benchmark function for each LLM and each prompt
+    #     create_benchmark(llm_model, prompt_function_name) # WHOLE BENCHMARK
+# 
+    # print("\n✅ All benchmarks have been generated successfully!")
 
 if __name__ == "__main__":
     main()
