85e9bf3b91dfb0ef8e969c5a12adbc2c1a17d1e6
francescacarlon <st185989@stud.uni-stuttgart.de>
2025-03-15 10:11:15 +0100

    added html texts

--- linguistic_analysis.py
+++ linguistic_analysis.py
@@ -1,26 +1,20 @@
 """
 File with the linguistic metrics functions.
 For each text:
+- length (token count);
 - readability: Flesch scores;
 - POS distribution.
 
 For similarity scores between texts:
-- Jaccard similarity;
 - Cosine similarity;
-- BLEU and ROUGE scores.
+- Jaccard similarity;
+- BLEU and ROUGE scores. 
 """
 
 import nltk
 import textstat
-#import numpy as np
-#from sklearn.feature_extraction.text import CountVectorizer
-#from sklearn.metrics.pairwise import cosine_similarity
-#from nltk.translate.bleu_score import sentence_bleu
-#from rouge_score import rouge_scorer
 from collections import Counter
-#
-# Download necessary NLTK resources if not already available
-#nltk.download('punkt_tab')
+
 nltk.download('averaged_perceptron_tagger_eng')
 
 # Function to compute tokens: length of the text
@@ -82,33 +76,6 @@ def compute_pos_distribution(text):
     return pos_distribution
 
 
-"""
-# Function to compute Jaccard similarity between two texts
-def compute_jaccard_similarity(text1, text2):
-    set1 = set(nltk.word_tokenize(text1.lower()))
-    set2 = set(nltk.word_tokenize(text2.lower()))
-    intersection = len(set1.intersection(set2))
-    union = len(set1.union(set2))
-    return intersection / union if union else 0
-
-# Function to compute Cosine similarity with CountVectorizer
-def compute_cosine_similarity(text1, text2):
-    vectorizer = CountVectorizer().fit_transform([text1, text2])
-    vectors = vectorizer.toarray()
-    return cosine_similarity(vectors)[0, 1]
-
-# Function to compute BLEU score
-def compute_bleu(reference, candidate):
-    reference_tokens = [nltk.word_tokenize(reference.lower())]
-    candidate_tokens = nltk.word_tokenize(candidate.lower())
-    return sentence_bleu(reference_tokens, candidate_tokens)
-
-# Function to compute ROUGE scores
-def compute_rouge(reference, candidate):
-    scorer = rouge_scorer.RougeScorer(['rouge-1', 'rouge-2', 'rouge-l'], use_stemmer=True)
-    scores = scorer.score(reference, candidate)
-    return {key: value.fmeasure for key, value in scores.items()}
-    """
 
 # Function to analyze a text
 def analyze_text(text):
